948455872453fd7f7cacad56b6faf3bd8a164d7f
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-17 22:10:05 -0500

    Add dynamic content generation for about.html

---
+++ static/js/renderAbout.js
@@ -0,0 +1,28 @@
+import RenderModule from './renderModule.js';
+
+let body = document.querySelector('body');
+let mobile = window.innerWidth < 768;
+
+function removeChilds(body) {
+  const removeSponsor = document.querySelector('.sponsors') || '';
+  if (removeSponsor) body.removeChild(removeSponsor);
+  const removeFooter = document.querySelector('footer') || '';
+  if (removeFooter) body.removeChild(removeFooter);
+}
+
+function renderPage() {
+  body = document.querySelector('body');
+  mobile = window.innerWidth < 768;
+  removeChilds(body);
+  const docFrag = document.createDocumentFragment();
+  if (mobile) {
+    docFrag.appendChild(RenderModule.renderSponsorSec(mobile));
+    docFrag.appendChild(RenderModule.renderFoot(mobile));
+  } else {
+    docFrag.appendChild(RenderModule.renderFoot(mobile, false));
+  }
+  body.appendChild(docFrag);
+}
+
+window.addEventListener('load', renderPage);
+window.addEventListener('resize', renderPage);
